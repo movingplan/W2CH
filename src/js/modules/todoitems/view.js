0752245d4dd1dbf1dc0142e2fa90bedacdea5511
movingplan <45482995+movingplan@users.noreply.github.com>
2018-12-18 12:55:18 +0100

    test

--- src/js/modules/todoitems/view.js
+++ src/js/modules/todoitems/view.js
@@ -37,7 +37,7 @@ export default class extends app.View {
         });
 
         this.get("#todolist").innerHTML = list.join("");
-        
+        this.el = document.getElementById("todosection");
        
     }
 
