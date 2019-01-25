27696eccddf9eb3c84c12cb6e8428155c520d304
movingplan <45482995+movingplan@users.noreply.github.com>
2019-01-25 16:39:43 +0100

    move date

--- src/js/modules/todoitems/wix/sendMail.js
+++ src/js/modules/todoitems/wix/sendMail.js
@@ -1,14 +1,28 @@
-import { MainService } from "./main.service";
+import wixWindow from 'wix-window';
+import { local } from 'wix-storage';
+import { MessageHandlerService as MessageHandler } from 'public/todoitems/services/messagehandler.service.js'
+import wixUsers from 'wix-users';
+import wixLocation from 'wix-location';
+import wixData from 'wix-data'
+import { MainService } from 'public/todoitems/services/main.service.js'
 
-export async function button11_click(event) {
+$w.onReady(function () {
+	//TODO: write your page related code here...
+
+});
+export async function button1_click(event) {
 	try {
-        
-        let user = await wixUsers.promptLogin( {"mode": "signup"});
-        await wixUsers.emailUser('Verify', user.id, {	variables: { "approvalToken": user.id}});
-        await new MainService(this.getContext()).registerForApprovalAndTransfer(user);
+
+		let user = await wixUsers.promptLogin({ "mode": "signup", "lang": "de" });
+		console.log(`after promt`, user, wixWindow.lightbox.getContext());
+		let days = { days: 90, days_after_move: 0 };
+		let ms = new MainService(days);
+		await ms.registerForApprovalAndTransfer();
+		await wixUsers.emailUser('Verify', user.id, { variables: { "approvalToken": user.id } });
 		await wixLocation.to("/verification");
-		
-    } catch (err) {
-        console.log(`${err.message} ${err.stack}`)
-    }
+
+	} catch (err) {
+		console.log(`${err.message} ${err.stack}`)
+	}
+
 }
