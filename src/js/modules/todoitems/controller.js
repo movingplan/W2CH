2b3bec2d6ca886f75ad0a1896143fb80700937ab
movingplan <45482995+movingplan@users.noreply.github.com>
2019-02-27 11:36:37 +0100

    pdf generation

--- src/js/modules/todoitems/controller.js
+++ src/js/modules/todoitems/controller.js
@@ -169,6 +169,7 @@ export default class extends app.Controller {
             if (blob) {
                 console.log(`in blob`);
                 let file = new Blob([blob], { type: 'application/pdf' });
+                console.log(`blob ` + blob);
                 console.log(file);
                 var fileURL = URL.createObjectURL(file);
                 console.log(`file url reached`, fileURL);
